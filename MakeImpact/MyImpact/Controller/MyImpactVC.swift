619f5259b47c68bd0575fcee1927e5c4d561f04c
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-02 11:19:26 -0800

    [add] setup VC structure

--- MakeImpact/MyImpact/Controller/MyImpactVC.swift
+++ MakeImpact/MyImpact/Controller/MyImpactVC.swift
@@ -9,25 +9,26 @@
 import UIKit
 
 class MyImpactVC: UIViewController {
-
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
     
+//MARK: Private Methods
     fileprivate func setupViews() {
         view.backgroundColor = .black
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//MARK: IBActions
+    
+//MARK: Helpers
+    
 }
+
+//MARK: Extensions
+
