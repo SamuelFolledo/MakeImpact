648ae33a30a5b7b68e10441faf9eb186a5be7a51
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-02 12:57:35 -0800

    [add] MainTabBarController to initially load the DIscover tab first

---
+++ MakeImpact/Main/Controller/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/2/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2 //initially show Discover Tab first
+    }
+
+}
