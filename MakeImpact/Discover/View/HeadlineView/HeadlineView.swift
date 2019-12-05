faf41c59169e25fc40b3363039f8b63a7dfb6737
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-04 20:17:21 -0800

    [add] init HeadlineView swift file and made as xib's file owner

---
+++ MakeImpact/Discover/View/HeadlineView/HeadlineView.swift
@@ -0,0 +1,16 @@
+//
+//  HeadlineView.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/4/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class HeadlineView: UIView {
+    
+    
+    
+    
+}
