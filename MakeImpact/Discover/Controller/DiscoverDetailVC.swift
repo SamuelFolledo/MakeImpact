3cc9e64264d98de5c9e8d51d7c4c17dd74aebe57
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-04 22:01:52 -0800

    [add] DiscoverDetailVC swift file

---
+++ MakeImpact/Discover/Controller/DiscoverDetailVC.swift
@@ -0,0 +1,34 @@
+//
+//  DiscoverDetailVC.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/4/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverDetailVC: UIViewController {
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .blue
+    }
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
+
