596be2baada54711121be3493686ca33a718afec
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-08 15:36:22 -0800

    [add] news cell which is type UICollectionViewCell which replaced HeadLineView

---
+++ MakeImpact/Discover/View/DiscoverDetail/NewsCell/NewsCell.swift
@@ -0,0 +1,43 @@
+//
+//  NewsCell.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/8/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+//MARK: Properties
+    var issue: Issue! {
+        didSet {
+            setupView()
+        }
+    }
+    
+//MARK: IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setupView() {
+        imageView.image = issue.image
+        titleLabel.text = issue.mainCategory
+        descriptionLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes { //this is to have a dynamic height on cell
+        setNeedsLayout()
+        layoutIfNeeded()
+        let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+        var frame = layoutAttributes.frame
+        frame.size.height = ceil(size.height)
+        layoutAttributes.frame = frame
+        return layoutAttributes
+    }
+}
