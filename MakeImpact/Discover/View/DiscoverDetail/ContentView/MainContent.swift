af4f63b795e5609dad4b2fb88cc4eea73b1dfd9f
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-08 15:36:35 -0800

    [add] another cell for main content

---
+++ MakeImpact/Discover/View/DiscoverDetail/ContentView/MainContent.swift
@@ -0,0 +1,21 @@
+//
+//  MainContent.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/8/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MainContent: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
