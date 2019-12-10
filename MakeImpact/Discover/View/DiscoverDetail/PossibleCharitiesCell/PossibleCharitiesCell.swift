05d403b698f032c489d7294e2a9aeb4d4d8d0c48
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-10 08:07:38 -0800

    [qip] possible charities which isn't properly constrained yet

---
+++ MakeImpact/Discover/View/DiscoverDetail/PossibleCharitiesCell/PossibleCharitiesCell.swift
@@ -0,0 +1,18 @@
+//
+//  PossibleCharitiesCell.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/9/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class PossibleCharitiesCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
