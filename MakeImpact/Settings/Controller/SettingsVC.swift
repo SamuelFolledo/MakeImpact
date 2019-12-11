50af6c0e1463c1d3a60fc27653fc17ed0e4b0363
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-11 11:14:03 -0800

    [add] implemented the new way to populate CellData for Settings

--- MakeImpact/Settings/Controller/SettingsVC.swift
+++ MakeImpact/Settings/Controller/SettingsVC.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class SettingsVC: UIViewController {
 //MARK: Properties
-    var datas: [String: UIImage]!
+    var settings: [SettingData]!
+    
 //MARK: IBOutlets
     @IBOutlet weak var tableView: UITableView!
     
@@ -23,21 +24,23 @@ class SettingsVC: UIViewController {
 //MARK: Private Methods
     fileprivate func setupViews() {
         createCellData()
-        view.backgroundColor = .blue
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(UINib(nibName: "SettingsCell", bundle: nil), forCellReuseIdentifier: kSETTINGSCELLID)
+        
     }
     
     fileprivate func createCellData() {
-        datas = [
-            "Profile": kBLANKIMAGE,
-            "Update Photo": kBLANKIMAGE,
-            "Account Settings": kBLANKIMAGE,
-            "My Activities": kBLANKIMAGE,
-            "Tax Info": kBLANKIMAGE,
-            "Settings": kBLANKIMAGE,
-            "Credits": kBLANKIMAGE,
-            "Logout": kBLANKIMAGE
+        settings = [
+            SettingData(title: "Profile", image: kBLANKIMAGE),
+            SettingData(title: "Update Photo", image: kBLANKIMAGE),
+            SettingData(title: "Account Settings", image: kBLANKIMAGE),
+            SettingData(title: "My Activities", image: kBLANKIMAGE),
+            SettingData(title: "Tax Info", image: kBLANKIMAGE),
+            SettingData(title: "Settings", image: kBLANKIMAGE),
+            SettingData(title: "Credits", image: kBLANKIMAGE),
+            SettingData(title: "Logout", image: kBLANKIMAGE)
+            
         ]
     }
     
@@ -54,13 +57,12 @@ extension SettingsVC: UITableViewDelegate {
 
 extension SettingsVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        settings.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: kSETTINGSCELLID, for: indexPath)
-        cell.song = datas[indexPath.row]
-        cell.populateCell()
+        let cell: SettingsCell = tableView.dequeueReusableCell(withIdentifier: kSETTINGSCELLID, for: indexPath) as! SettingsCell
+        cell.setting = settings[indexPath.row]
         return cell
     }
     
