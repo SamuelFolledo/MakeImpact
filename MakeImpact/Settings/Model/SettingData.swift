e725e3fd921e6169a541b136856b912acd207c7b
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-11 11:04:15 -0800

    [add] struct for setting data

---
+++ MakeImpact/Settings/Model/SettingData.swift
@@ -0,0 +1,14 @@
+//
+//  SettingData.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/11/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+struct SettingData {
+    var title: String
+    var image: UIImage
+}
