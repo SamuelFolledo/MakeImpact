295ccd919a709101e1ca9496d33a113cd9d92ec1
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-14 23:09:51 -0800

    [add] created charity model

---
+++ MakeImpact/Discover/View/DiscoverDetailViews/PossibleCharitiesCell/CharityModel/Charity.swift
@@ -0,0 +1,14 @@
+//
+//  Charity.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/14/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
+
+struct Charity {
+    var title: String
+    var image: UIImage
+}
