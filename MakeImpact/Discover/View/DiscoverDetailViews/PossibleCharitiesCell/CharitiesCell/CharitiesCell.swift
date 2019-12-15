eb82c2c40eea10d30da1e89e9ab184c6ff09c06f
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-14 22:42:45 -0800

    [add] cells for each charity in possible charities

---
+++ MakeImpact/Discover/View/DiscoverDetailViews/PossibleCharitiesCell/CharitiesCell/CharitiesCell.swift
@@ -0,0 +1,18 @@
+//
+//  CharitiesCell.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/14/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class CharitiesCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
