728a11969fb4720f2be6099570952311ffd69947
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-14 22:03:42 -0800

    [add] create VC file for settings details

---
+++ MakeImpact/Settings/Controller/SettingsDetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsDetailVC.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/14/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsDetailVC: UIViewController {
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .black
+    }
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
