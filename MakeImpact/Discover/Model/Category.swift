01e9129c7ad2ae4a81d606afb63bfe5e6163eeb4
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-21 01:26:38 -0500

    [add] Category class

---
+++ MakeImpact/Discover/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  MakeImpact
+//
+//  Created by Macbook Pro 15 on 12/21/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class Category {
+    
+}
