190991595a8ef4b32511efb19216edfed0426e8a
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-21 22:57:06 -0500

    [add] all current cases available in Category

--- MakeImpact/Discover/Model/Category.swift
+++ MakeImpact/Discover/Model/Category.swift
@@ -9,5 +9,56 @@
 import UIKit
 
 enum Category: String {
+/*
+    - Categories for Issues and Charities
+    - To add a new category, add the new case, add string value in CategoryConstant.swift, update in text computed property
+*/
     
+//Impact the world
+    case worldHunger, poverty,
+//Technology
+    womenInTech, technology, innovative,
+//Recycling
+    recycle, reusableEnergy,
+//Save Earth
+    earth, forest, endangeredSpecies, globalWarming,
+//Politics
+    politics,
+//Area
+    local, national, worldwide
+    
+    var text: String {
+        switch self {
+        case .worldHunger:
+            return kWORLDHUNGER
+        case .poverty:
+        return kPOVERTY
+        case .womenInTech:
+            return kWOMENINTECH
+        case .technology:
+            return kTECHNOLOGY
+        case .innovative:
+            return kINNOVATIVE
+        case .recycle:
+            return kRECYCLE
+        case .reusableEnergy:
+            return kREUSABLEENERGY
+        case .forest:
+            return kFOREST
+        case .earth:
+            return kEARTH
+        case .endangeredSpecies:
+            return kENDANGEREDSPECIES
+        case .globalWarming:
+            return kGLOBALWARMING
+        case .politics:
+            return kPOLITICS
+        case .local:
+            return kLOCAL
+        case .national:
+            return kNATIONAL
+        case .worldwide:
+            return kWORLDWIDE
+        }
+    }
 }
